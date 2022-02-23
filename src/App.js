b7f6c008c149a9ee94ce1d522a9cf3312be34cf0
Michael <miwanek11199@gmail.com>
2022-02-22 20:32:56 -0800

    added starter code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+import Board from './components/Board';
+
+const PLAYER_1 = 'X';
+const PLAYER_2 = 'O';
+
+const generateSquares = () => {
+  const squares = [];
+
+  let currentId = 0;
+
+  for (let row = 0; row < 3; row += 1) {
+    squares.push([]);
+    for (let col = 0; col < 3; col += 1) {
+      squares[row].push({
+        id: currentId,
+        value: '',
+      });
+      currentId += 1;
+    }
+  }
+
+  return squares;
+};
+
+const App = () => {
+  // This starts state off as a 2D array of JS objects with
+  // empty value and unique ids.
+  const [squares, setSquares] = useState(generateSquares());
+
+  // Wave 2
+  // You will need to create a method to change the square
+  //   When it is clicked on.
+  //   Then pass it into the squares as a callback
+
+  const checkForWinner = () => {
+    // Complete in Wave 3
+    // You will need to:
+    // 1. Go accross each row to see if
+    //    3 squares in the same row match
+    //    i.e. same value
+    // 2. Go down each column to see if
+    //    3 squares in each column match
+    // 3. Go across each diagonal to see if
+    //    all three squares have the same value.
+  };
+
+  const resetGame = () => {
+    // Complete in Wave 4
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Tic Tac Toe</h1>
+        <h2>The winner is ... -- Fill in for wave 3 </h2>
+        <button>Reset Game</button>
       </header>
+      <main>
+        <Board squares={squares} />
+      </main>
     </div>
   );
-}
+};
 
 export default App;
